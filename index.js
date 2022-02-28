a3fe238f8d9d3ca5e1e28e345b0cbc73507d0ab7
Khuat Duc Khanh <khuat.duc.khanh@azoom.jp>
2022-02-28 06:37:39 +0000

    :+1: Implement API

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import 'dotenv/config'
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/health", function (req, res) {
+  return res.sendStatus(200);
+});
+
+app.get("/secret", function (req, res) {
+  const secret = process.env.SECRET;
+
+  return res.send(secret);
+});
+
+app.listen(port, function () {
+  console.log("App listening on port " + port);
+});
